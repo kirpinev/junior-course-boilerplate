99c98512117c3a13d3839bd9251e4c3eebf787dd
Igor Kirpinev <kirpinev@yahoo.com>
2021-06-14 12:48:29 +0300

    Update ProductsList

--- src/components/ProductsList/index.jsx
+++ src/components/ProductsList/index.jsx
@@ -3,21 +3,27 @@ import ProductCard from 'csssr-school-product-card';
 import { Icons } from '../../Icons';
 import { List } from '../../uikit';
 
-export const ProductsList = ({ products }) => (
-  <List>
-    {products.map(item => (
-      <List.Item key={item.id}>
-        <ProductCard
-          isInStock={item.isInStock}
-          img={item.img}
-          title={item.title}
-          price={item.price}
-          subPriceContent={item.subPriceContent}
-          maxRating={5}
-          rating={item.rating}
-          ratingComponent={Icons.Star}
-        />
-      </List.Item>
-    ))}
-  </List>
-);
+export const ProductsList = ({ products = [] }) => {
+  if (products.length === 0) {
+    return <p>Список товаров пуст</p>;
+  }
+
+  return (
+    <List>
+      {products.map(item => (
+        <List.Item key={item.id}>
+          <ProductCard
+            isInStock={item.isInStock}
+            img={item.img}
+            title={item.title}
+            price={item.price}
+            subPriceContent={item.subPriceContent}
+            maxRating={5}
+            rating={item.rating}
+            ratingComponent={Icons.Star}
+          />
+        </List.Item>
+      ))}
+    </List>
+  );
+};
