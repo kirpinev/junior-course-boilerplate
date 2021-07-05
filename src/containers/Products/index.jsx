9a382fc301eb38481625a11ed26b7661a5d5bb2f
Igor Kirpinev <kirpinev@yahoo.com>
2021-07-05 19:21:56 +0300

    Update products

--- src/containers/Products/index.jsx
+++ src/containers/Products/index.jsx
@@ -112,7 +112,7 @@ export class Products extends Component {
     }
   };
 
-  handleFiltersDelete = () => {
+  handleFiltersReset = () => {
     this.setState({
       min: defaultMinPrice,
       max: defaultMaxPrice,
